d5114def6d8449a3ec650d6b70f3099acaa3bd55
jhpark <mugenjh@gmail.com>
2020-02-23 22:24:59 +0900

    CNN

---
+++ chap05/cnn.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from history_plot import history_plot
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
+
+train_X = train_X / 255.0
+test_X = test_X / 255.0
+
+print(train_X.shape, test_X.shape)
+
+train_X = train_X.reshape(-1, 28, 28, 1)
+test_X = test_X.reshape(-1, 28, 28, 1)
+
+# reshape 이후
+print(train_X.shape, test_X.shape)
+
+plt.figure(figsize=(10, 10))
+for c in range(16):
+    plt.subplot(4, 4, c + 1)
+    plt.imshow(train_X[c].reshape(28, 28), cmap='gray')
+
+plt.show()
+
+print(train_Y[:16])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(input_shape=(28, 28, 1), kernel_size=(3, 3), filters=16),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=32),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=64),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=128, activation='relu'),
+    tf.keras.layers.Dense(units=10, activation='softmax')
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(train_X, train_Y, epochs=25, validation_split=0.25)
+history_plot(history)
+
+model.evaluate(test_X, test_Y, verbose=0)
